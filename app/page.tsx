635f997d477c7dfc90d36d7c31b5b0211c9e257b
kasimugur <kasiugur7@gmail.com>
2024-11-28 15:41:35 +0300

    list countries on homepage #3

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+'use client'
+import CountryDetail from "@/components/CountryDetail";
 import HeaderPage from "../components/Header";
+import CountryCard from "@/components/CountryCard";
 
 export default function Home() {
+// console.log(data.map(e => e))
   return (
     <>
-    <HeaderPage />
+      <HeaderPage />
+      <div className="grid gap-16 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  mx-5">
+      <CountryCard />
+      </div>
+      {/* <CountryDetail />  */}
     </>
   );
 }
--- components/CountryDetail.tsx
+++ components/CountryDetail.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
-
+'use client'
+import React, { useEffect } from 'react'
+import data from "@/data.json"
+import Image from 'next/image'
 export default function CountryDetail() {
+
+  useEffect(()=>{},[])
+  const imageCountry = data.filter(i => i.flag)
   return (
-    <div>CountryDetail</div>
+    <>
+    <div className="flex">
+
+      <div className="flex flex-col">
+      {data.map(e=> (
+        // <h1>{e.flag}</h1>
+        <Image src={e.flag}  width={300} height={300} alt={e.name} />
+        
+      ))}
+      </div>
+    </div>
+    </>
   )
 }
