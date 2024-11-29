987d120213877cf8d9aecc5eff896cd1dbb09960
kasimugur <kasiugur7@gmail.com>
2024-11-29 10:00:41 +0300

    detail page will be made #2

--- app/(routes)/detail/[slug]/page.tsx
+++ app/(routes)/detail/[slug]/page.tsx
@@ -1,7 +1,17 @@
+'use client'
 import React from 'react'
+import data from '@/data.json'
+import { useParams } from 'next/navigation'
+import CountryDetail from '@/components/CountryDetail';
+import { Box } from '@mui/material';
 
 export default function DetailCountryPage() {
+  const { slug } = useParams();
+  console.log(data.filter(e => e.numericCode == slug).map(e => e.name))
+  console.log(typeof (slug))
   return (
-    <div>DetailCountryPage</div>
+    <Box component={'div'}>
+      <CountryDetail slug={slug} />
+    </Box>
   )
 }
--- app/(routes)/detail/page.tsx
+++ app/(routes)/detail/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function DetailPage() {
   return (
-    <div>DetailPage</div>
+    <>
+
+    </>
   )
 }
