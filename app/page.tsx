63d1d5e69fa5c7b8291f2a9b572778c697a6bb48
kasimugur <kasiugur7@gmail.com>
2024-11-30 15:01:07 +0300

    Filtering countries on the home page #4

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
 'use client'
-import CountryDetail from "@/components/CountryDetail";
 import HeaderPage from "../components/Header";
 import CountryCard from "@/components/CountryCard";
-
+import { Box } from "@mui/material";
+import SearchBar from "@/components/SearchBar";
+import data from "@/data.json"
+import { useRoutesContext } from "./context/RoutesContext";
 export default function Home() {
-// console.log(data.map(e => e))
+  const { filterData }:any = useRoutesContext()
+  const filterRegion = filterData !== null ? data.filter(e  => e.region === filterData) : data
+
+  console.log("filter data ", filterRegion )
   return (
     <>
       <HeaderPage />
-      <div className="grid gap-16 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  mx-5">
-      <CountryCard />
-      </div>
-      {/* <CountryDetail />  */}
+      <SearchBar />
+      <Box component={'div'} className="grid gap-16 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  mx-5">
+        {
+          filterRegion.map(e => <CountryCard  key={e.numericCode} e={e} /> )
+        }
+      </Box>
     </>
   );
 }
