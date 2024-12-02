09fc562e175f93c39e72142bbb36aca0db3d7daf
kasimugur <kasiugur7@gmail.com>
2024-12-02 20:04:59 +0300

    dark theme and light theme will be set and header added #5

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,36 @@
 'use client'
 import HeaderPage from "../components/Header";
 import CountryCard from "@/components/CountryCard";
-import { Box } from "@mui/material";
+import { Box, Container, createTheme, ThemeProvider } from "@mui/material";
 import SearchBar from "@/components/SearchBar";
 import { useRoutesContext } from "./context/RoutesContext";
 export default function Home() {
   const { filterData, inputSearch }: any = useRoutesContext()
-  const filterRegion = filterData !== null ? inputSearch.filter(e => e.region === filterData) : inputSearch
+  const filterCountry = filterData !== null ? inputSearch.filter(e => e.region === filterData) : inputSearch
 
+  const theme = createTheme({
+    colorSchemes: {
+      dark: true,
+    },
+  });
   return (
     <>
-      <HeaderPage />
-      <SearchBar />
-      <Box component={'div'} className="grid gap-16 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  mx-5">
-        {
-          filterRegion.map(e => <CountryCard key={e.numericCode} e={e} />)
-        }
-      </Box>
+      <ThemeProvider theme={theme}>
+        <Container className="flex flex-col gap-4" maxWidth="lg">
+          <Box className="mb-5" component={"div"}>
+
+            <HeaderPage />
+          </Box>
+          <Box  component={"div"}>
+            <SearchBar />
+          </Box>
+          <Box component={'div'} className="grid gap-16 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-8">
+            {
+              filterCountry.map(e => <CountryCard key={e.numericCode} e={e} />)
+            }
+          </Box>
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import ThemaModal from './ThemaModal'
+import { Typography } from '@mui/material'
 
 export default function HeaderPage() {
   return (
-    <div>HeaderPage</div>
+    <div className='dark:bg-rest-darkBlue bg-rest-white  flex justify-between w-full p-4'>
+      <Typography variant='h5'  className=' text-rest-veryTextBlue dark:text-rest-white '>Where in the world?</Typography>
+      <ThemaModal />
+    </div>
   )
 }
